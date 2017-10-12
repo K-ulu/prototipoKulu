90c3d731c805aad2cc24c983e95981154adeadc2
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2017-10-12 01:04:58 -0500

    Probando React en el proyecto

--- client/main.js
+++ client/main.js
@@ -1,22 +1,6 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Meteor.startup(() => {
+  ReactDOM.render(<h1>Hello World!!</h1>, document.getElementById('app'));
 });
