bca3cb90ffae5ff89ac6f7f4f24dd9fad729867e
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2017-11-03 17:44:22 -0500

    Ajustes con bootstrap

--- client/main.js
+++ client/main.js
@@ -1,17 +1,17 @@
 import { Meteor } from 'meteor/meteor';
 import ReactDOM from 'react-dom';
-
+//import '../imports/compatibility/bootstrap.js';
 import { routes } from '../imports/routes/routes';
 
 // Import Tether.js - Bootstrap tooltip requires Tether.
 // We also add 'global.Tether' as a workaround for Meteor.
-import tether from 'tether';
-global.Tether = tether;
+//import tether from 'tether';
+//global.Tether = tether;
 
-import popper from 'popper.js';
-global.Popper = popper;
+//import popper from 'popper.js';
+//global.Popper = popper;
 // Import Bootstrap js npm module.cmd
-bootstrap = require('bootstrap');
+//bootstrap = require('bootstrap');
 
 Meteor.startup(() => {
   ReactDOM.render(routes, document.getElementById('app'));
