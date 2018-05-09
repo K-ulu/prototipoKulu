13468b6aa87c67eca11fd720ec172c5a3007ba42
carojg <carojg@users.noreply.github.com>
2018-05-09 11:29:06 -0500

    cambioCaro

--- imports/ui/Caro.js
+++ imports/ui/Caro.js
@@ -1,6 +1,19 @@
 import React from 'react'
+import {Docente} from '../../server/Docente'
 
 export default class Caro extends React.Component {
+    constructor(props){
+        let total = Docente.find().count();
+        super(props);
+        this.state = {
+          error: '', //almacena el error
+          value: '' //almacena valor del tipo de usuario
+        };
+    
+        //contexto de navegador para nuestras funciones
+        this.handleChange = this.handleChange.bind(this);    
+        this.onSubmit = this.onSubmit.bind(this);
+      }
 
 
     render(){
